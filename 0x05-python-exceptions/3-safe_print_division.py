9879fc3957ca001640296ca1eedf7c56cb6dd0fc
Offiong Ekpenyong <93795678+Offiongwhilley@users.noreply.github.com>
2022-06-20 18:38:10 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
