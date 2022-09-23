79a06430b1187761532ede502c7e514028423a8b
Offiongwhilley <offiongwhilley@gmail.com>
2022-09-23 08:55:02 -0700

    correction

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and finally displays the body of the response
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email,
+and displays the body of the response
 """
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
+
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
